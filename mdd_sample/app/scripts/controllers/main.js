b72beffc6f9b9f9e9fcc7dc895d17a21cf7d191f
Jeanna <jawa@fullsail.edu>
2014-04-10 01:19:03 -0400

    committing angular port
    routing working

---
+++ mdd_sample/app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('mddSampleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
